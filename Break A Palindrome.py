8366d330aa4558fc5c395e3aeee9ec3ec14c7a65
Karthikayan Mailsamy <54244642+karthikayan4u@users.noreply.github.com>
2021-09-24 17:15:10 +0530

    Create Break A Palindrome.py
    Day - 23

---
+++ Break A Palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) == 1:
+            return ''
+        s = list(palindrome)
+        for i in range(len(s) // 2):
+            if s[i] > 'a':
+                s[i] = 'a'
+                return ''.join(s)
+        s[-1] = 'b'
+        return ''.join(s)
